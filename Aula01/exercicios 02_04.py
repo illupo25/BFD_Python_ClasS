93f02b615b44b4e317722fa0ae3c8ae99e6f032e
fabio <fabinhotucano@gmail.com>
2025-08-14 01:25:54 +0000

    hj

--- Aula01/exercicios 02_04.py
+++ Aula01/exercicios 02_04.py
@@ -1,3 +1,9 @@
-int(input("Digite um numero"))
-for x in range(1,10):
-    print(x)
+num1 = float(input("Digite o primeiro numero "))
+num2 = float(input("Digite o segundo numero "))
+
+if num1 > num2:
+    print(f"O maior e o {num1}")
+elif num2 > num1:
+    print(f"O maior e o {num2}")
+else:
+    print("os dois numeros sao iguais")
